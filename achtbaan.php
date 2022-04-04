9da68c87651ab9b5926f5d230ff112c4cead40df
Vin Valentino <42009589+VinValentinoo@users.noreply.github.com>
2022-04-04 14:23:38 +0200

    Aanpassingen voor de pagina en head.php verwijderd
    Head.php verwijderd, omdat dit toch niet handig was. Misschien dat dit later terug komt

--- achtbaan.php
+++ achtbaan.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require_once('head.php'); ?>
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/css/achtbaan.css">
     <title>Achtbaan</title>
 </head>
 <body>
-    
+    <h1> De constructie </h1>
+    <p> Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn. </p>
 </body>
 </html>
