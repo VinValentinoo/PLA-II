0d0861f35d654f8ee40535e633db640dae06fcb6
Vin Valentino <42009589+VinValentinoo@users.noreply.github.com>
2022-04-04 14:16:24 +0200

    Pagina's toegevoegd en een require.
    Achtbaan.php toegevoegd, head.php toegevoegd waar de stylesheet in gelinkt staat die je gewoon kan requiren. Kleine aanpassing in de index.php

--- achtbaan.php
+++ achtbaan.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once('head.php') ?>
-    <title>Home page</title>
+    <title>Achtbaan</title>
 </head>
 <body>
     
