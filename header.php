eec952cb87ca2bd58ceaa66cfa2c889be7daae16
calthan leander <calsonleander99@gmail.com>
2022-04-06 11:46:12 +0200

    added the html of header.php
    DONE  #3

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+	<div class="navbar">
+		<a href="index.php">HOME</a>
+		<a href="achtbaan.php">CONSTRUCTIE</a>
+		<a href="karretjes.php">KARRETJES</a>
+	</div>
+	<img src="img/Logo.png" width="50px" height="50px" alt="logo">
+</header>
