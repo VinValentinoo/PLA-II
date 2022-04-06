0e141742db4b9d446816cab4cb215235af42326b
calthan leander <calsonleander99@gmail.com>
2022-04-06 12:16:23 +0200

    added html and css index.php
    FIX #14 FIX #11

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <?php require_once 'head.php'; ?>
     <title>Home page</title>
 </head>
 <body>
-    
+    <?php require_once 'header.php'; ?>
+    <main>
+        <div class="homePage">
+            <div class="wrapper">
+                <h1>HOME</h1> 
+                
+            </div>
+        </div>
+        <div class="homePageText">
+            <div class="wrapper">
+                <h2>Onze techniek</h2>
+                <p>Onze gloednieuwe achtbaantechniek heet MagnaDrive. </p>
+                <p>Door middel van een aantal onderdelen die ons ontwikkelteam uitgevonden heeft, kunnen we een achtbaan produceren die tot 5-10% sneller kan gaan dan de snelste achtbaan tot nu toe, enkele graden scherpte kan toevoegen aan bochten, en ook nog langer meegaat dan achtbanen gemaakt van gebruikelijke onderdelen.</p>
+                <p>Het mooiste is nog dat de onderdelen niet veel duurder zullen zijn dan onze concurrentie. Vanwege de scherpere bochten is in veel gevallen ook de achtbaan compacter te maken, wat ook weer ruimte vrijmaakt voor andere attracties.</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
