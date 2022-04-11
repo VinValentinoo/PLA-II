75c87428dcd8d1f3d192e2f8503dc1fac6710923
lukasdeVries <lukaspdevries@gmail.com>
2022-04-11 14:32:47 +0200

    Create karretjes.php
    ik heb de basis van de karretjes pafina in html geschreven.

---
+++ karretjes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>karretjes</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+        <?php 
+
+        require_once "header.php"
+
+        ?>
+        <main>
+            <div class="cartPageImage">
+                <div class="wrapper">
+                    <h1>Karretjes</h1>
+                </div>
+            </div>
+            <div class="cartPageText">
+                <h2>Onze Karren</h2>
+                <p>door middel van panelen kunnen we uitgebrijde personalisatie bieden aan onze klanten</p>
+            </div>
+        </main>
+</body>
+</html>
