21d0f26253c323f60b3b2f05da159fbe916a1c31
avihar94 <Aviharhangi61@hotmail.com>
2022-04-11 14:20:14 +0200

    flex toegepast

--- contactpage.php
+++ contactpage.php
@@ -8,17 +8,18 @@
 </head>
 <body>
     <h1>contactpage</h1>
-    <form>
-        <label for="name">naam:</label>
-        <input type="text" id="name" name="name" placeholder="typ hier uw naam">
-        <label for="achternaam">achternaam:</label>
-        <input type="text" id="surname" name="surname" placeholder="typ hier uw achternaam">
-        <label for="title">titel:</label>
-        <input type="text" id="title" name="title" placeholder="vul hier uw onderwerp in">
-        <label for="message">uw klacht/recensie:</label>
-        <input for="textarea" id=message name="message" placeholder="typ hier uw bericht">
-    </form>
-        
+        <div class="form">
+            <form>
+                <label for="name">naam:</label>
+                <input type="text" id="name" name="name" placeholder="typ hier uw naam">
+                <label for="achternaam">achternaam:</label>
+                <input type="text" id="surname" name="surname" placeholder="typ hier uw achternaam">
+                <label for="title">titel:</label>
+                <input type="text" id="title" name="title" placeholder="vul hier uw onderwerp in">
+                <label for="message">uw klacht/recensie:</label>
+                <input for="textarea" id=message name="message" placeholder="typ hier uw bericht">
+            </form>
+        </div>
 
     
 </body>
