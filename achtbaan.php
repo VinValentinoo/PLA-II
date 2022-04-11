f7cdddce3960239ccf438920cc56405f9573c889
calthan leander <calsonleander99@gmail.com>
2022-04-11 14:39:17 +0200

    merged achtbaan css with main css

--- achtbaan.php
+++ achtbaan.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/achtbaan.css" type="text/css">
+    <?php
+    require_once('head.php');
+    ?>
     <title>Achtbaan</title>
 </head>
 <body>
     <?php
     require_once('header.php');
-    require_once('head.php');
     ?>
     <div class="container-background">
         <div class="container-titel">
