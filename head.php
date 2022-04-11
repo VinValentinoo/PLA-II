d3ea459679b1eb9c07f0710decb9b3311bfeb5d2
Vin Valentino <42009589+VinValentinoo@users.noreply.github.com>
2022-04-11 14:43:50 +0200

    footer afgemaakt

--- head.php
+++ head.php
@@ -2,3 +2,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
 <link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/footer.css">
