57d67deb06c8e3ef9058f2a90ae441a44bb24bb7
Vin Valentino <42009589+VinValentinoo@users.noreply.github.com>
2022-04-11 14:25:51 +0200

    achtbaan pagina helemaal af en toegevoegd
    achtbaan pagina helemaal af en toegevoegd

--- achtbaan.php
+++ achtbaan.php
@@ -8,11 +8,22 @@
     <title>Achtbaan</title>
 </head>
 <body>
-    <div class="container-titel">
-        <h1> De constructie </h1>
+    <?php
+    require_once('header.php');
+    require_once('head.php');
+    ?>
+    <div class="container-background">
+        <div class="container-titel">
+            <h1> Constructie </h1>
+        </div>
     </div>
-    <div class="container-text">
-        <p> Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn. </p>
-    </div>
+        <div class="container-text">
+            <div class="container">
+                <div class="container-text-titel">
+                    <h2> De constructie </h2>
+                </div>
+                <p> Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn. </p>
+            </div>
+        </div>
 </body>
 </html>
